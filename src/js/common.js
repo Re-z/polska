042729a8ff57dbd6e5e0776a3817c542a806770d
Re-z <re-z@ukr.net>
2016-11-16 23:24:15 +0200

    index started

---
+++ src/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$('.recalls__slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        focusOnSelect: true
+    })
+
+});
