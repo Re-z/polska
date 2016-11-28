8e0dbec9fa0910e93f9b94ec31b2c16b620daa9f
Re-z <re-z@ukr.net>
2016-11-28 23:17:47 +0200

    contacts.php fixed

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
+    //feedback script
+    document.getElementById('feedback').onsubmit = function(){
+        var http = new XMLHttpRequest();
+        http.open("POST", "contacts2.php", true);
+        http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        http.send("nameFF=" + this.nameFF.value + "&contactFF=" + this.contactFF.value + "&messageFF=" + this.messageFF.value);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                alert(http.responseText + ', Ваше сообщение получено!.\nАдминистрация ответит на него на протяжении 2х дней.\nСпасибо!');
+            }
+        };
+        http.onerror = function() {
+            alert('Извините, данные не передано');
+        };
+        return false;
+    }
+
+
+
 	$('.recalls__slider').slick({
         dots: true,
         arrows: false,
@@ -52,21 +71,6 @@ function initMapSec() {
 }
 initMapSec();
 
-//feedback script
-    document.getElementById('feedback').onsubmit = function(){
-        var http = new XMLHttpRequest();
-        http.open("POST", "contacts2.php", true);
-        http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        http.send("nameFF=" + this.nameFF.value + "&contactFF=" + this.contactFF.value + "&messageFF=" + this.messageFF.value);
-        http.onreadystatechange = function() {
-            if (http.readyState == 4 && http.status == 200) {
-                alert(http.responseText + ', Ваше сообщение получено!.\nАдминистрация ответит на него на протяжении 2х дней.\nСпасибо!');
-            }
-        };
-        http.onerror = function() {
-            alert('Извините, данные не передано');
-        };
-        return false;
-    }
+
 
 });
