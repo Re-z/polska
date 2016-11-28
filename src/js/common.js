44afb70ded3359bc80e02a700a21b38e8482ff9f
Re-z <re-z@ukr.net>
2016-11-28 21:57:10 +0200

    contacts.php tuning

--- src/js/common.js
+++ src/js/common.js
@@ -52,4 +52,22 @@ function initMapSec() {
 }
 initMapSec();
 
+//feedback script
+    document.getElementById('feedback').onsubmit = function(){
+        var http = new XMLHttpRequest();
+        http.open("POST", "contacts.php", true);
+        http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        http.send("nameFF=" + this.nameFF.value + "&contactFF=" + this.contactFF.value + "&messageFF=" + this.messageFF.value);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                alert(http.responseText + ', Ваше сообщение получено!.\nАдминистрация ответит на него на протяжении 2х дней.\nСпасибо!');
+            }
+        };
+        http.onerror = function() {
+            alert('Извините, данные не передано');
+        };
+        return false;
+
+    }
+
 });
